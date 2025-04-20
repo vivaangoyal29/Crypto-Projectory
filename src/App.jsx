f4338becd7558fbe4d386694250a26e5369dc996
Vivaan Goyal <samgoyal.2006@gmail.com>
2025-04-20 22:16:19 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Coin from './pages/Coin/COin'
+import Footer from './components/footer/footer'
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/coin/:coinId' element={<Coin />}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @2025, Cryptoplace - All Rights Reserved </p>
+    </div>
+  )
+}
+
+export default Footer
