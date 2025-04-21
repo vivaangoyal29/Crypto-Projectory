fcf93061ca7d4e58ac422764dd4e91e6ebedd142
Vivaan Goyal <samgoyal.2006@gmail.com>
2025-04-21 21:09:13 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import COin from './pages/Coin/COin'
 import Footer from './components/footer/footer'
+import Coin from './pages/Coin/COin'
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/coin/:coinId' element={<COin />}/>
+        <Route path='/coin/:coinId' element={<Coin />}/>
       </Routes>
       <Footer/>
     </div>
