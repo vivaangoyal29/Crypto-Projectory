da6e2d9c11d9f4387e00279537aea56349c520d3
Vivaan Goyal <samgoyal.2006@gmail.com>
2025-04-21 20:57:02 +0530

    File changed to prevent deployment issues

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState, useContext} from 'react'
-import './Coin.css'
+import './COin.css'
 import {useParams} from 'react-router-dom'
 import {CoinContext} from '../../context/CoinContext'
 import LineChart from '../../components/LineChart/LineChart'
 
-const Coin = () => {
+const COin = () => {
     const {coinId} = useParams();
     const [coinData, setCoinData] = useState();
     const [historicalData, setHistoricalData] = useState()
@@ -85,4 +85,4 @@ const Coin = () => {
     }
 }
 
-export default Coin
+export default COin
